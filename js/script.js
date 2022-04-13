beac91d82af0e782b9ccc94b18e9028eba70cb21
DLoCode <Derriklopez1992@gmail.com>
2022-04-13 14:12:17 -0700

    Update script.js
    replaced basic code, and included an array of pokemon and information to reference later in the pokedex app

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
-alert('Hello World');
-let favoriteFood = 'Sopes';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "charmander" , height: 27 , types: ["fire" , "flying"]},
+  {name: "squirtle" , height: 18 , types: "water"},
+  {name: "bulbasaur" , height: 16 , types: "grass"}
+];
