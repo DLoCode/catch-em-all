e2eee9bc1425025e8d853d263eff94635150084c
DLoCode <Derriklopez1992@gmail.com>
2022-04-16 12:20:52 -0700

    Update script.js
    added a for loop referencing the list of pokemon, displaying the name, and height. nested a conditional inside the loop, and added code to highlight the size of a big pokemon.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ let pokemonList = [
   {name: "squirtle" , height: 18 , types: "water"},
   {name: "bulbasaur" , height: 16 , types: "grass"}
 ];
+
+// for loop
+for (let i = 0; i < pokemonList.length; i++){
+  // nested if else if conditionals below
+  // references the pokemon from list and displays height
+  if (pokemonList[i].height > 25) {
+    // also adds a size threshold for big pokemon classification
+    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " WOW! That's a big pokemon! ");
+  }else if (pokemonList[i].height >=20 && pokemonList[i].height <=25) {
+    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " " + " is an average size pokemon ");
+  }else {
+    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " ");
+  }
+}
