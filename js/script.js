433e1feff7fdea7db3f3edfa4ef9f9aa8df4e26a
DLoCode <Derriklopez1992@gmail.com>
2022-04-26 21:55:32 -0700

    Update script.js
    cleaned up code for organization and readability, replaced the basic for loop with a forEach loop with nested if else if conditions.

--- js/script.js
+++ js/script.js
@@ -4,16 +4,14 @@ let pokemonList = [
   {name: "bulbasaur" , height: 16 , types: "grass"}
 ];
 
-// for loop
-for (let i = 0; i < pokemonList.length; i++){
-  // nested if else if conditionals below
-  // references the pokemon from list and displays height
-  if (pokemonList[i].height > 25) {
-    // also adds a size threshold for big pokemon classification
-    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " WOW! That's a big pokemon! ");
-  }else if (pokemonList[i].height >=20 && pokemonList[i].height <=25) {
-    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " " + " is an average size pokemon ");
+// forEach loop
+pokemonList.forEach(function (pokemon) {
+// if - else if - else conditionals
+  if (pokemon.height > 25) {
+    document.write(pokemon.name + " " + "height: " + pokemon.height + " " + " WOW! That's a bit pokemon! ");
+  }else if (pokemon.height >= 20 && pokemon.height <= 25) {
+    document.write(pokemon.name + " " + "height: " + pokemon.height + " " + " is an average sized pokemon ");
   }else {
-    document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + " ");
+    document.write(pokemon.name + " " + "height: " + pokemon.height);
   }
-}
+});
