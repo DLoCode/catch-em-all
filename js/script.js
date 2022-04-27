c1980377a2ce8b0a77cadb0629c1c2859c7d2e7c
DLoCode <Derriklopez1992@gmail.com>
2022-04-27 14:23:57 -0700

    Update script.js
    added iife format for accessing updated states of pokemonList inside iife, created a forEach loop to iterate over the list inside iife to log all items to console

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
+// global state
 let pokemonList = [
   {name: "charmander" , height: 27 , types: ["fire" , "flying"]},
   {name: "squirtle" , height: 18 , types: "water"},
   {name: "bulbasaur" , height: 16 , types: "grass"}
 ];
+// iife format
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: "charmander" , height: 27 , types: ["fire" , "flying"]},
+    {name: "squirtle" , height: 18 , types: "water"},
+    {name: "bulbasaur" , height: 16 , types: "grass"}
+  ];
+// definined value of pokemonRepository
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    }
+  };
+})();
 
-// forEach loop
-pokemonList.forEach(function (pokemon) {
-// if - else if - else conditionals
-  if (pokemon.height > 25) {
-    document.write(pokemon.name + " " + "height: " + pokemon.height + " " + " WOW! That's a bit pokemon! ");
-  }else if (pokemon.height >= 20 && pokemon.height <= 25) {
-    document.write(pokemon.name + " " + "height: " + pokemon.height + " " + " is an average sized pokemon ");
-  }else {
-    document.write(pokemon.name + " " + "height: " + pokemon.height);
-  }
-});
+pokemonRepository.add({name: 'Pikachu', height: 12, types: 'electric'});
+console.log(pokemonRepository.getAll());
+// forEach loop iterating over list inside iife
+pokemonList.forEach(console.log(pokemonRepository.getAll()))
