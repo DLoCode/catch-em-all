9d513f7527cc1af284099d35ac80f4e8263a190b
DLoCode <Derriklopez1992@gmail.com>
2022-05-03 22:55:22 -0700

    Update script.js
    added a function to create a button for each pokemon in my list, and added an event listener to log the pokemons name to console when button is clicked.

--- js/script.js
+++ js/script.js
@@ -11,18 +11,57 @@ let pokemonRepository = (function() {
     {name: "squirtle" , height: 18 , types: "water"},
     {name: "bulbasaur" , height: 16 , types: "grass"}
   ];
-// definined value of pokemonRepository
-  return {
-    add: function(pokemon) {
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
       pokemonList.push(pokemon);
-    },
-    getAll: function () {
-      return pokemonList;
+    } else {
+      console.log("incorrect pokemon");
     }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  function showDetails(pokemon) {
+    let name = pokemon.name;
+    console.log(name);
+  }
+  // add a pokemon function
+  function addListItem(pokemon) {
+    //selecting the container of the pokemonList
+    let pokemonList = document.querySelector(".pokemon-list");
+    //creating an li element inside the ul parent
+    let listpokemon = document.createElement("li");
+    // creating a button for each pokemon
+    let button = document.createElement("button");
+    // setting the inner text of each button
+    button.innerText = pokemon.name;
+    // adding the class poke-button to each created button
+    button.classList.add("poke-button");
+    // putting the button inside the li element
+    listpokemon.appendChild(button);
+    // putting li element and its contents inside the ul
+    pokemonList.appendChild(listpokemon);
+    // event listerner for 'click' to log pokemon information
+    button.addEventListener('click',function(event) {
+      showDetails(pokemon);
+    });
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
 pokemonRepository.add({name: 'Pikachu', height: 12, types: 'electric'});
 console.log(pokemonRepository.getAll());
 // forEach loop iterating over list inside iife
-pokemonList.forEach(console.log(pokemonRepository.getAll()))
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
